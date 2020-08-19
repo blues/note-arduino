86007c479f90d4510d7618ed0be9d52e6902a813
Brandon Satrom <bsatrom+gh@gmail.com>
2020-08-19 09:48:10 -0500

    Squashed 'src/note-c/' content from commit 606c73b
    git-subtree-dir: src/note-c
    git-subtree-split: 606c73ba3fcf69829ab15c38ea61ebe432a05af1

---
+++ n_const.c
@@ -0,0 +1,16 @@
+// Copyright 2018 Inca Roads LLC.  All rights reserved.
+// Use of this source code is governed by licenses granted by the
+// copyright holder including that found in the LICENSE file.
+
+#include "n_lib.h"
+
+const char *c_null = "null";
+const char *c_false = "false";
+const char *c_true = "true";
+const char *c_nullstring = "";
+const char *c_newline = "\n";
+const char *c_mem = "mem";
+const char *c_timeout = "timeout";
+const char *c_err = "err";
+const char *c_req = "req";
+const char *c_bad = "bad";
