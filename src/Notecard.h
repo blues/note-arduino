2d3b52514471efbdb835fe9a600fe715f54ddbe8
Zachary J. Fields <zachary_fields@yahoo.com>
2021-02-10 14:25:16 -0600

    feat: define Dx digital pin values (#37)

--- src/Notecard.h
+++ src/Notecard.h
@@ -36,6 +36,50 @@
 #include "mock/mock-parameters.hpp"
 #endif
 
+#ifdef ARDUINO_ARCH_ESP32
+
+#ifdef B0
+#undef B0
+#endif
+#define B0 21
+
+#ifdef D5
+#undef D5
+#endif
+#define D5 14
+
+#ifdef D6
+#undef D6
+#endif
+#define D6 32
+
+#ifdef D9
+#undef D9
+#endif
+#define D9 15
+
+#ifdef D10
+#undef D10
+#endif
+#define D10 33
+
+#ifdef D11
+#undef D11
+#endif
+#define D11 27
+
+#ifdef D12
+#undef D12
+#endif
+#define D12 12
+
+#ifdef D13
+#undef D13
+#endif
+#define D13 13
+
+#endif
+
 /**************************************************************************/
 /*!
     @brief  Class that stores state and functions for interacting with the
