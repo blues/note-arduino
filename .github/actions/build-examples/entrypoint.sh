26aeb18769cb75309cda21f84096db252093a167
Matthew McGowan <mat.mcgowan+git@gmail.com>
2021-08-19 21:52:02 -0700

    chore: uses workflow matrix to run multiple platforms in parallel and avoid duplication of steps. Docker based on ubuntu.

--- .github/actions/build-examples/entrypoint.sh
+++ .github/actions/build-examples/entrypoint.sh
@@ -1,9 +1,17 @@
 # ensure commands that return non-zero cause the script to fail
 set -e
 
+boards_default=arduino:avr:uno
+platforms_default=arduino:avr
+
+boards=${BOARDS:-$boards_default}
+cores=${PLATFORMS:-$platforms_default}
+
+echo "Building library examples for $cores ($boards)"
+
 # not sure why this is needed since it's already included in the dockerfile. Possibly changes to the current user
-cores="arduino:nrf52 arduino:avr arduino:samd arduino:mbed_rp2040"
-boards="arduino:mbed_rp2040:pico arduino:avr:uno"
+#cores="arduino:nrf52 arduino:avr arduino:samd arduino:mbed_rp2040"
+#boards="arduino:mbed_rp2040:pico arduino:avr:uno"
 # arduino:nrf52:primo and esp32 fail with a g++ error arm-none-eabi-g++: obstack_free: symbol not found
 # https://github.com/esphome/esphome/issues/19#issuecomment-392430924
 # May be simplest to move to ubuntu as the base docker image
@@ -14,16 +22,12 @@ exclude=" arduino:mbed_rp2040:pico-./examples/Example2_PeriodicCommunications/Ex
 
 
 # WORKSPACE is either the github workspace or the cwd if not defined.
-export WORKSPACE=${GITHUB_WORKSPACE:-$(pwd)}
+export WORKSPACE=${GITHUB_WORKSPACE:-/note-arduino}
 [ ! -d $ARDUINO/libraries/note-arduino ] && ln -s $WORKSPACE $ARDUINO/libraries/note-arduino
 echo "Add arduino library path $WORKSPACE"
 
-arduino-cli config init
 # add ESP32 support - https://www.survivingwithandroid.com/arduino-cli-compile-upload-manage-libraries-cores-boards/
-arduino-cli config add board_manager.additional_urls "https://raw.githubusercontent.com/espressif/arduino-
-esp32/gh-pages/package_esp32_index.json"
-# required for ESP32 support
-apk add bash libstdc++
+# arduino-cli config add board_manager.additional_urls "https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
 
 arduino-cli core update-index
 
