48e121fc4950680209327a000bff33ca619ce595
Matthew McGowan <mat.mcgowan+git@gmail.com>
2021-10-19 14:51:13 +0200

    add code without coverage to verify that a decrease in coverage fails the build.

---
+++ src/Not_Covered.cpp
@@ -0,0 +1,23 @@
+// This file is deliberately not covered. The PR can be thrown away once we validate that coveralls rejects it and accepts it when the code is removed. 
+
+unsigned int crc32b(unsigned char *message) {
+   int i, j;
+   unsigned int byte, crc, mask;
+
+   i = 0;
+   crc = 0xFFFFFFFF;
+   while (message[i] != 0) {
+      byte = message[i];            // Get next byte.
+      crc = crc ^ byte;
+      for (j = 7; j >= 0; j--) {    // Do eight times.
+         mask = -(crc & 1);
+         crc = (crc >> 1) ^ (0xEDB88320 & mask);
+      }
+      i = i + 1;
+   }
+   return ~crc;
+}
+
+void f(void) {
+    
+}
