87a36c6ad7e2f81392f88bb1497aa5afd4ddaacd
Matthew McGowan <mat.mcgowan+git@gmail.com>
2021-10-20 13:29:12 +0200

    add more untested code

--- src/Not_Covered.cpp
+++ src/Not_Covered.cpp
@@ -18,6 +18,26 @@ unsigned int crc32b(unsigned char *message) {
    return ~crc;
 }
 
+
+unsigned int crc32b_2(unsigned char *message) {
+   int i, j;
+   unsigned int byte, crc, mask;
+
+   i = 0;
+   crc = 0xFFFFFFFF;
+   while (message[i] != 0) {
+      byte = message[i];            // Get next byte.
+      crc = crc ^ byte;
+      for (j = 7; j >= 0; j--) {    // Do eight times.
+         mask = -(crc & 1);
+         crc = (crc >> 1) ^ (0xEDB88320 & mask);
+      }
+      i = i + 1;
+   }
+   return ~crc;
+}
+
+
 void f(void) {
-    
+
 }
