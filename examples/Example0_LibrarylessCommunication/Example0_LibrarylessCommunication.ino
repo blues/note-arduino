acf6baf516af3107fc7a0cb3c52d774afd4238df
Zachary J. Fields <zachary_fields@yahoo.com>
2022-05-25 18:49:02 -0500

    feat: Compile for Adafruit nRF2840 Express

--- examples/Example0_LibrarylessCommunication/Example0_LibrarylessCommunication.ino
+++ examples/Example0_LibrarylessCommunication/Example0_LibrarylessCommunication.ino
@@ -24,6 +24,10 @@
 #define serialNotecard Serial1
 #endif
 
+#ifdef ARDUINO_NRF52840_FEATHER
+#include "Adafruit_TinyUSB.h"
+#endif
+
 // This is the unique Product Identifier for your device.  This Product ID tells the Notecard what
 // type of device has embedded the Notecard, and by extension which vendor or customer is in charge
 // of "managing" it.  In order to set this value, you must first register with notehub.io and
