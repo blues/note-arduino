e95340ed07de28e0f055c7b2d94b801cc18339d5
Zachary J. Fields <zachary_fields@yahoo.com>
2023-03-31 13:32:21 -0500

    fix: Example3

--- examples/Example3_InboundPolling/Example3_InboundPolling.ino
+++ examples/Example3_InboundPolling/Example3_InboundPolling.ino
@@ -1,34 +1,39 @@
-//
 // Copyright 2019 Blues Inc.  All rights reserved.
+//
 // Use of this source code is governed by licenses granted by the
 // copyright holder including that found in the LICENSE file.
 //
-// This example shows the simplest possible method demonstrating how a device might poll a
-// notefile used as an "inbound queue", using it to receive messages sent to the device from
-// the service.  The message gets into the service by use of the Notehub's HTTP/HTTPS inbound
-// request capability.
+// This example shows the simplest possible method demonstrating how a device
+// might poll a notefile used as an "inbound queue", using it to receive
+// messages sent to the device from the service.  The message gets into the
+// service by use of the Notehub's HTTP/HTTPS inbound request capability.
 //
 // In order to use this example,
-// 1. Get the device up and running the code below, successfully connecting to the servie
-// 2. Use the "Devices" view on notehub.io to determine the DeviceUID of the device, which
-//    is a unique string that looks like "dev:000000000000000"
-// 3. Use the "Settings / Project" view on notehub.io to determine the App UID of your project,
-//    a unique string that looks like "app:00000000-0000-0000-0000-000000000000"
-// 4. At the command line of your PC, send an HTTP message to the service such as:
+// 1. Get the device up and running the code below, successfully connecting to
+//    the servie
+// 2. Use the "Devices" view on notehub.io to determine the DeviceUID of the
+//    device, which is a unique string that looks like "dev:000000000000000"
+// 3. Use the "Settings / Project" view on notehub.io to determine the App UID
+//    of your project, a unique string that looks like
+//    "app:00000000-0000-0000-0000-000000000000"
+// 4. At the command line of your PC, send an HTTP message to the service
+//    such as:
 //    curl -L 'http://api.notefile.net/req?project="app:00000000-0000-0000-0000-000000000000"&device="dev:000000000000000"' -d '{"req":"note.add","file":"my-inbound.qi","body":{"my-request-type":"my-request"}}'
-//
 
 #include <Notecard.h>
-#include <Wire.h>
 
 // Parameters for this example
 #define INBOUND_QUEUE_POLL_SECS   10
 #define INBOUND_QUEUE_NOTEFILE    "my-inbound.qi"
 #define INBOUND_QUEUE_COMMAND_FIELD "my-request-type"
 
-// Note that both of these definitions are optional; just prefix either line with // to remove it.
-//  Remove txRxPinsSerial if you wired your Notecard using I2C SDA/SCL pins instead of serial RX/TX
-//  Remove usbSerial if you don't want the Notecard library to output debug information
+// Note that both of these definitions are optional; just prefix either line
+// with `//` to remove it.
+// - Remove txRxPinsSerial if you wired your Notecard using I2C SDA/SCL pins
+//   instead of serial RX/TX
+// - Remove usbSerial if you don't want the Notecard library to output debug
+//   information
+
 // #define txRxPinsSerial Serial1
 #define usbSerial Serial
 
@@ -46,9 +51,12 @@ Notecard notecard;
 void setup()
 {
 
-    // Set up for debug output.
+    // Set up for debug output (if available).
 #ifdef usbSerial
-    delay(2500);
+    // If you open Arduino's serial terminal window, you'll be able to watch
+    // JSON objects being transferred to and from the Notecard for each request.
+    const size_t usb_timeout_ms = 3000;
+    for (const size_t start_ms = millis() ; !usbSerial && (millis() - start_ms) < usb_timeout_ms ;);
     usbSerial.begin(115200);
     notecard.setDebugOutputStream(usbSerial);
 #endif
@@ -57,53 +65,54 @@ void setup()
 #ifdef txRxPinsSerial
     notecard.begin(txRxPinsSerial, 9600);
 #else
-    Wire.begin();
-
     notecard.begin();
 #endif
 
-    // Configure the productUID, and instruct the Notecard to stay connected to the service
+    // Configure the productUID, and instruct the Notecard to stay connected to
+    // the service if `myLiveDemo` is `true`.
     J *req = notecard.newRequest("hub.set");
     if (myProductID[0]) {
         JAddStringToObject(req, "product", myProductID);
     }
 #if myLiveDemo
     JAddStringToObject(req, "mode", "continuous");
-    JAddBoolToObject(req, "sync", true);  // Automatically sync when changes are made on notehub
+    JAddBoolToObject(req, "sync", true);  // Automatically sync when changes are
+                                          // made on notehub
 #else
     JAddStringToObject(req, "mode", "periodic");
     JAddNumberToObject(req, "inbound", 60);
 #endif
-    notecard.sendRequest(req);
-
+    notecard.sendRequestWithRetry(req, 5);
 }
 
-// In the Arduino main loop which is called repeatedly, add outbound data every 15 seconds
+// In the Arduino main loop which is called repeatedly, add outbound data every
+// 15 seconds
 void loop()
 {
-
-    // On a periodic basis, check the inbound queue for messages.  In a real-world application,
-    // this would be checked using a frequency commensurate with the required inbound responsiveness.
-    // For the most common "periodic" mode applications, this might be daily or weekly.  In this
-    // example, where we are using "continuous" mode, we check quite often for demonstratio purposes.
+    // On a periodic basis, check the inbound queue for messages.  In a
+    // real-world application, this would be checked using a frequency
+    // commensurate with the required inbound responsiveness. For the most
+    // common "periodic" mode applications, this might be daily or weekly.  In
+    // this example, where we are using "continuous" mode, we check quite often
+    // for demonstratio purposes.
     static unsigned long nextPollMs = 0;
     if (millis() > nextPollMs) {
         nextPollMs = millis() + (INBOUND_QUEUE_POLL_SECS * 1000);
 
         // Process all pending inbound requests
         while (true) {
-
-            // Get the next available note from our inbound queue notefile, deleting it
+            // Get the next available note from our inbound queue notefile,
+            // deleting it
             J *req = notecard.newRequest("note.get");
             JAddStringToObject(req, "file", INBOUND_QUEUE_NOTEFILE);
             JAddBoolToObject(req, "delete", true);
             J *rsp = notecard.requestAndResponse(req);
             if (rsp != NULL) {
-
-                // If an error is returned, this means that no response is pending.  Note
-                // that it's expected that this might return either a "note does not exist"
-                // error if there are no pending inbound notes, or a "file does not exist" error
-                // if the inbound queue hasn't yet been created on the service.
+                // If an error is returned, this means that no response is
+                // pending.  Note that it's expected that this might return
+                // either a "note does not exist" error if there are no pending
+                // inbound notes, or a "file does not exist" error if the
+                // inbound queue hasn't yet been created on the service.
                 if (notecard.responseError(rsp)) {
                     notecard.deleteResponse(rsp);
                     break;
@@ -118,7 +127,6 @@ void loop()
                     notecard.logDebug(JGetString(body, INBOUND_QUEUE_COMMAND_FIELD));
                     notecard.logDebug("\n\n");
                 }
-
             }
             notecard.deleteResponse(rsp);
         }
