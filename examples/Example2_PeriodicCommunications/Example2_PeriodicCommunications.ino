4587ca6657158a9a2f4372f65c23e9ad9acb7c40
Zachary J. Fields <zachary_fields@yahoo.com>
2023-03-31 13:25:22 -0500

    fix: Example2

--- examples/Example2_PeriodicCommunications/Example2_PeriodicCommunications.ino
+++ examples/Example2_PeriodicCommunications/Example2_PeriodicCommunications.ino
@@ -1,12 +1,16 @@
-//
 // Copyright 2020 Blues Inc.  All rights reserved.
+//
 // Use of this source code is governed by licenses granted by the
 // copyright holder including that found in the LICENSE file.
 //
-// This example does the same function as the "using library" example, but rather than
-// keeping the modem turned on constantly this example demonstrates how a developer would
-// gather sensor measurements "offline", then perform uploads on a periodic basis.
-//
+// This example does the same function as the "using library" example, but
+// rather than keeping the modem turned on constantly this example demonstrates
+// how a developer would gather sensor measurements "offline", then perform
+// uploads on a periodic basis.
+
+// Include the Arduino library for the Notecard
+#include <Notecard.h>
+#include <Notecarrier.h>
 
 #define ledPin              LED_BUILTIN
 
@@ -44,14 +48,13 @@
   #define buttonPin 3
 #endif
 
-// Include the Arduino library for the Notecard
-#include <Notecard.h>
-#include <Notecarrier.h>
-#include <Wire.h>
+// Note that both of these definitions are optional; just prefix either line
+// with `//` to remove it.
+// - Remove txRxPinsSerial if you wired your Notecard using I2C SDA/SCL pins
+//   instead of serial RX/TX
+// - Remove usbSerial if you don't want the Notecard library to output debug
+//   information
 
-// Note that both of these definitions are optional; just prefix either line with // to remove it.
-//  Remove txRxPinsSerial if you wired your Notecard using I2C SDA/SCL pins instead of serial RX/TX
-//  Remove usbSerial if you don't want the Notecard library to output debug information
 // #define txRxPinsSerial Serial1
 #define usbSerial Serial
 
@@ -78,11 +81,12 @@ void setup()
     pinMode(ledPin, OUTPUT);
     pinMode(buttonPin, buttonPressedState == LOW ? INPUT_PULLUP : INPUT);
 
-    // During development, set up for debug output from the Notecard.  Note that the initial delay is
-    // required by some Arduino cards before debug UART output can be successfully displayed in the
-    // Arduino IDE, including the Adafruit Feather nRF52840 Express.
+    // Set up for debug output (if available).
 #ifdef usbSerial
-    delay(2500);
+    // If you open Arduino's serial terminal window, you'll be able to watch
+    // JSON objects being transferred to and from the Notecard for each request.
+    const size_t usb_timeout_ms = 3000;
+    for (const size_t start_ms = millis() ; !usbSerial && (millis() - start_ms) < usb_timeout_ms ;);
     usbSerial.begin(115200);
     notecard.setDebugOutputStream(usbSerial);
 #endif
@@ -91,40 +95,42 @@ void setup()
 #ifdef txRxPinsSerial
     notecard.begin(txRxPinsSerial, 9600);
 #else
-    Wire.begin();
-
     notecard.begin();
 #endif
 
     // Service configuration request
     J *req = notecard.newRequest("hub.set");
 
-    // This command (required) causes the data to be delivered to the Project on notehub.io that has claimed
-    // this Product ID.  (see above)
+    // This command (required) causes the data to be delivered to the Project on
+    // notehub.io that has claimed this Product ID (see above).
     if (myProductID[0]) {
         JAddStringToObject(req, "product", myProductID);
     }
-    // This sets the notecard's connectivity mode to periodic, rather than being continuously connected
+
+    // This sets the notecard's connectivity mode to "periodic", rather than
+    // being continuously connected.
     JAddStringToObject(req, "mode", "periodic");
 
-    // This parameter establishes how often, in minutes, the Notecard will check for data that is waiting to be
-    // uploaded to the service.  Generally this might be something like 60 minutes, or perhaps even
-    // 12 hours * 60 min = 720 min.  For the purpose of this demonstration, however, we'll set the
+    // This parameter establishes how often, in minutes, the Notecard will check
+    // for data that is waiting to be uploaded to the service. Generally this
+    // might be something like 60 minutes, or perhaps even 12 hours * 60 min =
+    // 720 min. For the purpose of this demonstration, however, we'll set the
     // period such that it checks for outgoing data at most every 2 minutes.
     JAddNumberToObject(req, "outbound", 2);
 
-    // This parameter establishes how often, in minutes, the Notecard will check for data that is waiting on the
-    // service to be downloaded to the Notecard.  Generally this might be 12, 24, or even 48 hours,
-    // however for the purpose of this demonstration we will connect to the service to check for
+    // This parameter establishes how often, in minutes, the Notecard will check
+    // for data that is waiting on the service to be downloaded to the Notecard.
+    //  Generally this might be 12, 24, or even 48 hours, however for the
+    // purpose of this demonstration we will connect to the service to check for
     // incoming data at least once every hour.
     JAddNumberToObject(req, "inbound", 60);
 
     // Issue the request
-    notecard.sendRequest(req);
-
+    notecard.sendRequestWithRetry(req, 5);  // 5 seconds
 }
 
-// In the Arduino main loop which is called repeatedly, add outbound data every 15 seconds
+// In the Arduino main loop which is called repeatedly, add outbound data every
+// 15 seconds
 void loop()
 {
     static unsigned long lastStatusMs = 0;
@@ -160,13 +166,15 @@ void loop()
     notecard.logDebug("performing sensor measurement\n");
     lastStatusMs = millis();
 
-    // Count the simulated measurements that we send to the cloud, and stop the demo before long.
+    // Count the simulated measurements that we send to the cloud, and stop the
+    // demo before long.
     static unsigned eventCounter = 0;
     if (eventCounter++ > 25) {
         return;
     }
 
-    // Read the notecard's current temperature and voltage, as simulated sensor measurements
+    // Read the notecard's current temperature and voltage, as simulated sensor
+    // measurements
     double temperature = 0;
     J *rsp = notecard.requestAndResponse(notecard.newRequest("card.temp"));
     if (rsp != NULL) {
@@ -180,8 +188,9 @@ void loop()
         notecard.deleteResponse(rsp);
     }
 
-    // Enqueue the measurement to the Notecard for transmission to the Notehub.  These measurements
-    // will be staged in the Notecard's flash memory until it's time to transmit them to the service.
+    // Enqueue the measurement to the Notecard for transmission to the Notehub.
+    // These measurements will be staged in the Notecard's flash memory until
+    // it's time to transmit them to the service.
     J *req = notecard.newRequest("note.add");
     if (req != NULL) {
         J *body = JCreateObject();
@@ -202,7 +211,6 @@ void loop()
 // Button handling
 int buttonPress()
 {
-
     // Detect the "press" transition
     static bool buttonBeingDebounced = false;
     int buttonState = digitalRead(buttonPin);
@@ -241,5 +249,4 @@ int buttonPress()
     }
 
     return (buttonDoublePress ? BUTTON_DOUBLEPRESS : BUTTON_PRESS);
-
 }
