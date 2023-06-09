5655608ca2b8b2fa4de747f1af825543726693c6
Zachary J. Fields <zachary_fields@yahoo.com>
2023-06-09 08:55:16 -0500

    Squashed 'src/note-c/' content from commit e52a4a1
    git-subtree-dir: src/note-c
    git-subtree-split: e52a4a1bcf993e2aab8158075a926bed3c01324d

---
+++ test/src/JGetArray_test.cpp
@@ -0,0 +1,56 @@
+/*!
+ * @file JGetArray_test.cpp
+ *
+ * Written by the Blues Inc. team.
+ *
+ * Copyright (c) 2023 Blues Inc. MIT License. Use of this source code is
+ * governed by licenses granted by the copyright holder including that found in
+ * the
+ * <a href="https://github.com/blues/note-c/blob/master/LICENSE">LICENSE</a>
+ * file.
+ *
+ */
+
+#ifdef NOTE_C_TEST
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "n_lib.h"
+
+namespace
+{
+
+TEST_CASE("JGetArray")
+{
+    NoteSetFnDefault(malloc, free, NULL, NULL);
+
+    const char field[] = "list";
+    J *json = JCreateObject();
+    REQUIRE(json != NULL);
+    J *list = JCreateArray();
+    REQUIRE(list != NULL);
+    JAddItemToObject(json, field, list);
+
+    SECTION("NULL JSON") {
+        CHECK(JGetArray(NULL, "") == NULL);
+    }
+
+    SECTION("Field not present") {
+        CHECK(JGetArray(json, "array") == NULL);
+    }
+
+    SECTION("Field isn't an array") {
+        JAddNumberToObject(json, "num", 5);
+        CHECK(JGetArray(json, "num") == NULL);
+    }
+
+    SECTION("Valid") {
+        CHECK(JGetArray(json, field) != NULL);
+    }
+
+    JDelete(json);
+}
+
+}
+
+#endif // TEST
