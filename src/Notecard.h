86304fdda6aa84169b6e8d8711683187e5615d27
Zachary J. Fields <zachary_fields@yahoo.com>
2025-02-21 13:11:34 -0600

    chore: Create `NoteDefines.h` (#134)
    Allows `#define`s to be shared across compilation units

--- src/Notecard.h
+++ src/Notecard.h
@@ -27,6 +27,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "NoteDefines.h"
 #include "NoteI2c.hpp"
 #include "NoteLog.hpp"
 #include "NoteSerial.hpp"
@@ -44,21 +45,6 @@
 #include "mock/mock-arduino.hpp"
 #include "mock/mock-parameters.hpp"
 #endif
-
-#define NOTE_ARDUINO_VERSION_MAJOR 1
-#define NOTE_ARDUINO_VERSION_MINOR 6
-#define NOTE_ARDUINO_VERSION_PATCH 3
-
-#define NOTE_ARDUINO_VERSION NOTE_C_STRINGIZE(NOTE_ARDUINO_VERSION_MAJOR) "." NOTE_C_STRINGIZE(NOTE_ARDUINO_VERSION_MINOR) "." NOTE_C_STRINGIZE(NOTE_ARDUINO_VERSION_PATCH)
-
-#if defined(__GNUC__) | defined(__clang__)
-    #define NOTE_ARDUINO_DEPRECATED __attribute__((__deprecated__))
-#elif defined(_MSC_VER)
-    #define NOTE_ARDUINO_DEPRECATED __declspec(deprecated)
-#else
-    #define NOTE_ARDUINO_DEPRECATED
-    #define NOTE_ARDUINO_NO_DEPRECATED_ATTR
-#endif // __GNUC__ || __clang__
 
 /**************************************************************************/
 /*!
